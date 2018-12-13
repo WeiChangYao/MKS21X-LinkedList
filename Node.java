3d1c35fb2c35d4fb92420297025337b732671b98
weichangyao <wyao1@stuy.edu>
2018-12-13 10:57:34 -0500

    added most of the codes for Node. Didn't test yet

--- Node.java
+++ Node.java
@@ -1,9 +1,32 @@
 class Node{
- private int data;
- private Node next,prev;
- public Node(int num, Node nextNum, Node prevNum){
-   data = num;
-   next = nextNum;
-   prev = prevNum;
- }
+  private int data;
+  private Node next,prev;
+  public Node(int num, Node nextNum, Node prevNum){
+    data = num;
+    next = nextNum;
+    prev = prevNum;
+  }
+  public Node next(){
+    return next;
+  }
+  public Node prev(){
+    return prev;
+  }
+  public void setNext(Node other){
+    next = other;
+  }
+  public void setPrev(Node other){
+    prev = other;
+  }
+  public Integer getData(){
+    return data;
+  }
+  public Integer setData(Integer i){
+    return data;
+    data = i;
+  }
+  public String toString(){
+    String str = "";
+    return str += data;
+  }
 }
