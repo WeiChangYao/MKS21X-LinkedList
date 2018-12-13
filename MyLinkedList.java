b6a1ab375a33c13d5b3eaa8484c2feaf8fe862e6
weichangyao <wyao1@stuy.edu>
2018-12-13 11:02:49 -0500

    started MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,16 +1,18 @@
 class MyLinkedList{
- private int size;
- private Node start,end;
+  private int size;
+  private Node start,end;
 
- public int size(){
-   return size;
- }
  
- public boolean add(int value){
-   size++;                       //size goes up
-   end = new Node (value, ;                  //
- }
+
+  public int size(){
+    return size;
+  }
  
- public String toString();
+  public boolean add(int value){
+    size++;                       //size goes up
+    end = new Node (value, ;                  //
+  }
+ 
+  public String toString();
   
 }
