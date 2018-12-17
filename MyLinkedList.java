cd64d09b823699aadf915e94e62f8d7e099315d4
wyao1 <wyao1@stuy.edu>
2018-12-17 01:09:51 -0500

    added amazing comments

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -8,22 +8,22 @@ class MyLinkedList{
     start = new Node(null,null,null);//starting node with nothing
   }
   
-  public int size(){
-    return size;
-  }
+  public int size(){          
+    return size;                   //returns size
+   }
  
-  public boolean add(Integer value){  //IF IT'S BLANK MAKE IT SET START TO THE VALUE?!?!?!?!?!?
-    Node current = start;
-    Node newNode = new Node(value, null, null);
-    while (current.next() != null){
-      current = current.next();
+  public boolean add(Integer value){  
+    Node current = start;            //start with start
+    Node newNode = new Node(value, null, null); //makes a new node with value
+    while (current.next() != null){  //dont stop until you reach the end
+      current = current.next();    //current turns into next node
     }
-    current.setNext(newNode);
-    newNode.setPrev(current);
-    size++;
-    if(start.getData() == null){
-      start = current;
-      start.setPrev(null);
+    current.setNext(newNode);      //makes current node point to new node
+    newNode.setPrev(current);      //makes new node point back
+    size++;                        //add to size
+    if(start.getData() == null){   //IF THERE'S NOTHING THERE!!!
+      start = current;             //start node turns into the new node
+      start.setPrev(null);         //new node has no previous node
     }
     return true;
   }
@@ -31,55 +31,55 @@ class MyLinkedList{
   
  
   public String toString(){
-    String str = "[";
-    Node current = start;
-    while (current != null){
-      if(current.getData() != null){
-      str += current.getData();
-      if(current.next() != null){
-        str+= ", ";
+    String str = "[";             //starts with [
+    Node current = start;         //start with first node
+    while (current != null){      //dont stop until you reach the end
+      if(current.getData() != null){  //as long as data is not nothing (because I made start null by default in the beginning)
+        str += current.getData();     //add to string
+        if(current.next() != null){   //add ", " as long as it's not the last node
+          str+= ", ";
+        }
       }
-      }
-      current = current.next();
+      current = current.next();    //move to next node
       
     }
-    str += "]";
-    return str;
+    str += "]";                  //] to close it off
+    return str;                  //return the computer's hard work
   }
   
-  public Node getNthNode(int index){  //PRIVATE THIS!!!!!
-    int i = 0;
-    Node current = start;
-    while (current != null){
-      if (i == index){
-        return current;
+  private Node getNthNode(int index){  
+    int i = 0;                   //start with 0
+    Node current = start;        //start with start node
+    while (current != null){     //as long as current isn't nothing
+      if (i == index){           //stop when counter is equal to index
+        return current;          
       }
-      current = current.next();
-      i++;
+      current = current.next();  //move on and add to counter
+      i++;                       
       
     }
-    return current;
+    return current;              //return current node
   }
 
   public Integer get(int index){
     if(index < 0) throw new IndexOutOfBoundsException();
     if(index > size-1) throw new IndexOutOfBoundsException();
-    return getNthNode(index).getData();
+    return getNthNode(index).getData();   //return data of that node
   }
   
   public Integer set(int index, Integer value){
     if(index < 0) throw new IndexOutOfBoundsException();
     if(index > size-1) throw new IndexOutOfBoundsException();
-    Integer a = getNthNode(index).getData();
-    getNthNode(index).setData(value);
-    return a;
+    Integer a = getNthNode(index).getData();  //save the data
+    getNthNode(index).setData(value);         //set node's value to new value
+    return a;                                 //return saved data
   }
   
-  public boolean contains(Integer value){
+  public boolean contains(Integer value){     
     Node current = start;
-    while (current != null){
-      if(value.equals(current.getData())){
-        return true;
+    while (current != null){              
+      if(value.equals(current.getData())){ //checks if data is value
+        return true;                       //as long as the node isn't blank
       }
       current = current.next();
     }
@@ -91,75 +91,65 @@ class MyLinkedList{
     Node current = start;
     while (current != null){
       if(value.equals(current.getData())){
-        return count;
-      }
-      count++;
+        return count;          //if value is equal to the node data
+      }                        //return count
+      count++;                 //else, move on and add to count
       current = current.next();
     }
-    return -1;
+    return -1;                 //-1 if it's not there
   }
   
   public void add(int index,Integer value){
     if(index < 0) throw new IndexOutOfBoundsException();
     if(index > size) throw new IndexOutOfBoundsException();
     if (index == 0){
-      addFirst(value);
+      addFirst(value);  //add to first if index is 0
     } 
-  //  if (index == size){
-  //    addLast(value);
-  //  }
+
     else{
-    size++;
-    Node newNode = new Node(value, null, null);
-    newNode.setNext(getNthNode(index));
-	  getNthNode(index-1).setNext(newNode); 
-    }
+    size++;             //add to size
+    Node newNode = new Node(value, null, null); //make new node with value
+    newNode.setNext(getNthNode(index));         //set new node's next to node at index
+	  getNthNode(index-1).setNext(newNode);       //set the thing before index node to new node
+    }                                           //FORGOT TO SET PREV
   }
   
- /* public void addLast(Integer value){
-    size++;
-    Node newNode = new Node(value, null, end);
-    if(end != null) {
-      end.setNext(newNode);
-    }
-    end = newNode;
-  } */
-  
+
   public void addFirst(Integer value){
-    size++;
-    Node newNode = new Node(value, start, null);
-    if (start != null){
-      start.setPrev(newNode);
+    size++;                                //add to size
+    Node newNode = new Node(value, start, null); //make new node that points to start
+    if (start != null){                   //if start is real
+      start.setPrev(newNode);             //make it point back
     }
-    start = newNode;
+    start = newNode;                      //new node is the new start
   }
 
   public Integer remove(int index){
     if(index < 0) throw new IndexOutOfBoundsException();
     if(index > size-1) throw new IndexOutOfBoundsException();
-    if(index == size-1){
+    if(index == size-1){           //size -1 is the last index
         return removeLast();
      }
     if(index == 0){
       return removeFirst();
     }
     else{
-    Integer removeReturn = getNthNode(index).getData();
-    getNthNode(index+1).setPrev(getNthNode(index-1));
-    getNthNode(index-1).setNext(getNthNode(index+1));
-    size--;
+    Integer removeReturn = getNthNode(index).getData();//save data at index 
+    getNthNode(index+1).setPrev(getNthNode(index-1));  //set node infront index point to node before index
+    getNthNode(index-1).setNext(getNthNode(index+1));  //set node before index point to node infront index
+    size--;                                            //minus size
     return removeReturn;
     }
   }
   
   public boolean remove(Integer value){
-   if (contains(value) == true){
-      if(indexOf(value) == size-1){
+   if (contains(value) == true){        //sees if the value's there
+      if(indexOf(value) == size-1){     //if value is last, removeLast
         removeLast();
         return true;
       }
      else{
-      remove(indexOf(value));
+      remove(indexOf(value));         //remove thing in value's index
       return true;
      }
     }
@@ -167,21 +157,21 @@ class MyLinkedList{
   } 
   
   public Integer removeLast(){
-    Node current = getNthNode(size-1);
-    Integer val = current.getData();
-    current = getNthNode(size-2);
-    current.setNext(null);
-    end = current;
-    size--;
+    Node current = getNthNode(size-1); //current is last node
+    Integer val = current.getData();   //save value
+    current = getNthNode(size-2);      //current is second to last node
+    current.setNext(null);             //it doesn't point to anything
+    end = current;                     //it's the new end node
+    size--;                            //decrease size
     return val;
   }
   
   public Integer removeFirst(){
-    Node current = start;
-    Integer val = start.getData();
-    current = start.next();
-    current.setPrev(null);
-    start = current;
+    Node current = start;              
+    Integer val = start.getData();    //save start data
+    current = start.next();           //current is second node
+    current.setPrev(null);            //it points back to nothing
+    start = current;                  //it's the new start node
     size--;
     return val;
   }
